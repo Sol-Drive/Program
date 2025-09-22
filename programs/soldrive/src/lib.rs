e6657d80297ab93cf6a2652eb5915812b7a28c37
BiMalxMe <journeyxbimal@gmail.com>
2025-09-22 12:27:13 +0545

    Feat : Approaching the Correct Approach

--- programs/soldrive/src/lib.rs
+++ programs/soldrive/src/lib.rs
@@ -1,11 +1,19 @@
 use anchor_lang::prelude::*;
 
-declare_id!("CxDoRt3Nt5z747KNW6vkVxvQQ7c2dHMmGmoWNmxejA3f");
+// This will be your program ID (we'll update it after building)
+declare_id!("11111111111111111111111111111111");
 
 #[program]
 pub mod soldrive {
     use super::*;
-
+    
+    // Our first simple instruction - just prints a message
+    pub fn hello_soldrive(ctx: Context<HelloSoldrive>) -> Result<()> {
+        msg!("Hello from SolDrive! ");
+        Ok(())
+    }
 }
 
+// Context for our hello instruction (empty for now)
 #[derive(Accounts)]
+pub struct HelloSoldrive {}
--- tests/soldrive.ts
+++ tests/soldrive.ts
@@ -1,3 +1,18 @@
+// import * as anchor from "@coral-xyz/anchor";
+// import { Program } from "@coral-xyz/anchor";
+// import { Soldrive } from "../target/types/soldrive";
 
+// describe("soldrive", () => {
+//   const provider = anchor.AnchorProvider.env();
+//   anchor.setProvider(provider);
 
-//Will be Doing soon
+//   const program = anchor.workspace.Soldrive as Program<Soldrive>;
+
+//   it("Hi", async () => {
+//     const tx = await program.methods
+//       .helloSoldrive()
+//       .rpc();
+    
+//     console.log("Transaction signature:", tx);
+//   });
+// });
