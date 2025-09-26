973654a8121cf521903c8ecc0b94013b45e54c8e
BiMalxMe <journeyxbimal@gmail.com>
2025-09-26 08:05:40 +0545

    Feat : Creating User Profile Logic

--- programs/soldrive/src/lib.rs
+++ programs/soldrive/src/lib.rs
@@ -9,6 +9,8 @@ pub mod soldrive {
     
     // Our first simple instruction - just prints a message
     pub fn hello_soldrive(ctx: Context<HelloSoldrive>) -> Result<()> {
+        let config = &ctx.accounts;
+        //just a simple placeholder and so other image insetion logic in the solana bc
         msg!("Hello from SolDrive! ");
         Ok(())
     }
@@ -22,6 +24,19 @@ pub mod soldrive {
         config.max_file_size = 1_073_741_824;   
         
         msg!("SolDrive initialized! Authority: {}", config.authority);
+        Ok(())
+    }
+
+    pub fn create_user_profile(ctx: Context<CreateUserProfile>) -> Result<()> {
+        let user_profile = &mut ctx.accounts.user_profile;
+        user_profile.owner = ctx.accounts.user.key();
+        user_profile.files_owned = 0;
+        user_profile.storage_used = 0;
+        user_profile.storage_paid_until = 0;
+        // Start with good reputation
+        user_profile.reputation_score = 100; 
+        
+        msg!("User profile created for: {}", ctx.accounts.user.key());
         Ok(())
     }
 }
@@ -46,6 +61,23 @@ pub struct Initialize<'info> {
     
     pub system_program: Program<'info, System>,
 }
+
+#[derive(Accounts)]
+pub struct CreateUserProfile<'info>{
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 32 + 8 + 8 + 8 + 4, // discriminator + owner + files + storage + paid_until + reputation
+        seeds = [b"user_profile", user.key().as_ref()],
+        bump
+    )]
+    pub user_profile: Account<'info, UserProfile>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    
+    pub system_program: Program<'info, System>,
+}
 #[account]
 pub struct SolDriveConfig {
      // Who can update settings
@@ -56,4 +88,19 @@ pub struct SolDriveConfig {
     pub storage_fee_per_gb: u64,  
      // Maximum file size allowed
     pub max_file_size: u64,      
+}
+
+// USER PRODILE DS
+
+#[account]
+pub struct UserProfile{
+     // User's wallet address
+    pub owner: Pubkey,   
+    // How many files they own        
+    pub files_owned: u64,         
+     // Total bytes they're storing
+    pub storage_used: u64,       
+    // When their storage payment expires
+    pub storage_paid_until: i64,  
+    pub reputation_score: u32, 
 }
